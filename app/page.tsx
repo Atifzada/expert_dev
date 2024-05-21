104609104186b71846d97d1547b518877a2ebb6b
Atif Zada <116461959+Atifzada@users.noreply.github.com>
2024-05-21 15:29:30 +0000

    Create Section8 of the app

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Section1 from "@/components/Section1";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
-import Section5 from "@/components/Section5"
-import Section6 from "@/components/Section6"
-import Section7 from "@/components/Section7"
+import Section5 from "@/components/Section5";
+import Section6 from "@/components/Section6";
+import Section7 from "@/components/Section7";
+import Section8 from "@/components/Section8";
 
 const page1 = () => {
   return (
@@ -17,6 +18,7 @@ const page1 = () => {
     <Section5 />
     <Section6 />
     <Section7 />
+    <Section8 />
     </>
   )
 }
