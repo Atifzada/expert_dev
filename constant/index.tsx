56ad633efe77e4f577b6de75ddb44ca8140d4ccb
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-05-23 17:02:04 +0500

    Reference done without pagination and minor animation issues

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,170 +1,171 @@
 // NAVIGATION
 
 export const NAV_LINK = [
-    { href: "", key: "about", label: "O nás" },
-    { href: "", key: "services", label: "Služby" },
-    { href: "", key: "reference", label: "Reference", sublinks: [
-        // { href: "", label: "Sublink 1" },
-        // { href: "", label: "Sublink 2" },
-        // { href: "", label: "Sublink 3" },
-        // { href: "", label: "Sublink 4" },
-        // { href: "", label: "Sublink 5" },
-        // { href: "", label: "Sublink 6" },
-        // Add more sublinks here
-      ]
-    },
-    { href: "", key: "blog", label: "Blog" },
-    { href: "", key: "career", label: "Kariéra" },
-    { href: "", key: "contact", label: "Kontakt" },
-  ];
+  { href: "", key: "about", label: "O nás" },
+  { href: "", key: "services", label: "Služby" },
+  {
+    href: "", key: "reference", label: "Reference", sublinks: [
+      // { href: "", label: "Sublink 1" },
+      // { href: "", label: "Sublink 2" },
+      // { href: "", label: "Sublink 3" },
+      // { href: "", label: "Sublink 4" },
+      // { href: "", label: "Sublink 5" },
+      // { href: "", label: "Sublink 6" },
+      // Add more sublinks here
+    ]
+  },
+  { href: "", key: "blog", label: "Blog" },
+  { href: "", key: "career", label: "Kariéra" },
+  { href: "", key: "contact", label: "Kontakt" },
+];
 
 // CUSTOM_TABS_SECTION_3
 export const CUSTOM_TABS_SECTION_3 = [
-    { href: "", key: "weby", label: "Weby" },
-    { href: "", key: "eshopy", label: "Eshopy" },
-    { href: "", key: "seo", label: "SEO" },
-    { href: "", key: "online-marketing", label: "Online marketing" },
-    { href: "", key: "grafika", label: "Grafika" },
-    { href: "", key: "vyvoj-aplikaci", label: "Vývoj aplikací" },
-  ];  
+  { href: "", key: "weby", label: "Weby" },
+  { href: "", key: "eshopy", label: "Eshopy" },
+  { href: "", key: "seo", label: "SEO" },
+  { href: "", key: "online-marketing", label: "Online marketing" },
+  { href: "", key: "grafika", label: "Grafika" },
+  { href: "", key: "vyvoj-aplikaci", label: "Vývoj aplikací" },
+];
 
 // CUSTOM_TABS_SECTION_3
 export const CUSTOM_TABS_SECTION_7 = [
-    { href: "", key: "Frontend", label: "Frontend" },
-    { href: "", key: "Backend", label: "Backend" },
-    { href: "", key: "Mobilní aplikace", label: "Mobilní aplikace" },
-    { href: "", key: "Open Source", label: "Open Source" },
-  ];  
+  { href: "", key: "Frontend", label: "Frontend" },
+  { href: "", key: "Backend", label: "Backend" },
+  { href: "", key: "Mobilní aplikace", label: "Mobilní aplikace" },
+  { href: "", key: "Open Source", label: "Open Source" },
+];
 
 
-  // Array of objects containing image URLs and titles
+// Array of objects containing image URLs and titles
 export const FRONTEND_techItems = [
   {
-      image: "/react.webp",
-      image2: "/react-white.webp",
-      title: "React"
+    image: "/react.webp",
+    image2: "/react-white.webp",
+    title: "React"
   },
   {
     image: "angular.webp",
     image2: "web3.webp",
     title: "Angular"
-},
+  },
   {
-      image: "vuejs.webp",
-      image2: "vuejs-white.webp",
-      title: "Vue"
+    image: "vuejs.webp",
+    image2: "vuejs-white.webp",
+    title: "Vue"
   },
 
   {
-      image: "html5.webp",
-      image2: "html5-white.webp",
-      title: "HTML5"
+    image: "html5.webp",
+    image2: "html5-white.webp",
+    title: "HTML5"
   },
   {
-      image: "next.webp",
-      image2: "next-white.webp",
-      title: "Next"
+    image: "next.webp",
+    image2: "next-white.webp",
+    title: "Next"
   },
   {
-      image: "gatsby.webp",
-      image2: "gatsby-white.webp",
-      title: "Gatsby"
+    image: "gatsby.webp",
+    image2: "gatsby-white.webp",
+    title: "Gatsby"
   },
   {
-      image: "nuxtjs.webp",
-      image2: "nuxtjs-white.webp",
-      title: "Nuxtjs"
+    image: "nuxtjs.webp",
+    image2: "nuxtjs-white.webp",
+    title: "Nuxtjs"
   },
   // Add more items as needed
 ];
 // Array of objects containing image URLs and titles
 export const BACKEND_techItems = [
   {
-      image: "/jsnode.webp",
-      image2: "/jsnode2.webp",
-      title: "Node"
+    image: "/jsnode.webp",
+    image2: "/jsnode2.webp",
+    title: "Node"
   },
   {
     image: "laravel.webp",
     image2: "laravel2.webp",
     title: "Laravel"
-},
+  },
   {
-      image: "python.webp",
-      image2: "python2.webp",
-      title: "Phyton"
+    image: "python.webp",
+    image2: "python2.webp",
+    title: "Phyton"
   },
 
   {
-      image: "golang.webp",
-      image2: "golang2.webp",
-      title: "Golang"
+    image: "golang.webp",
+    image2: "golang2.webp",
+    title: "Golang"
   },
   {
-      image: "ror.webp",
-      image2: "ror2.webp",
-      title: "ROR"
+    image: "ror.webp",
+    image2: "ror2.webp",
+    title: "ROR"
   },
   // Add more items as needed
 
 ];
-  export const MOBILE_APPLICATION_techItems = [
-    {
-        image: "/ios.webp",
-        image2: "/ios2.webp",
-        title: "iSO"
-    },
-    {
-      image: "kotlin.webp",
-      image2: "kotlin2.webp",
-      title: "Kotlin"
+export const MOBILE_APPLICATION_techItems = [
+  {
+    image: "/ios.webp",
+    image2: "/ios2.webp",
+    title: "iSO"
   },
-    {
-        image: "swift.webp",
-        image2: "swift2.webp",
-        title: "Swift"
-    },
-  
-    {
-        image: "android.webp",
-        image2: "android2.webp",
-        title: "Android"
-    },
-    {
-        image: "flutter.webp",
-        image2: "flutter2.webp",
-        title: "Flutter"
-    },
-    {
-        image: "java.webp",
-        image2: "java2.webp",
-        title: "Java"
-    },
-    {
-        image: "react.webp",
-        image2: "react-white.webp",
-        title: "React"
-    },
-    // Add more items as needed
-  ];
-  export const OPEN_SOURCE_techItems = [
-    {
-        image: "/wordpress.png",
-        image2: "/wordpress2.png",
-        title: "WordPress"
-    },
-    {
-      image: "prestashop.png",
-      image2: "prestashop2.png",
-      title: "Prestashop"
+  {
+    image: "kotlin.webp",
+    image2: "kotlin2.webp",
+    title: "Kotlin"
   },
-    {
-        image: "shopify.webp",
-        image2: "shopify2.webp",
-        title: "Shopify"
-    },
-    // Add more items as needed
-  ];
+  {
+    image: "swift.webp",
+    image2: "swift2.webp",
+    title: "Swift"
+  },
+
+  {
+    image: "android.webp",
+    image2: "android2.webp",
+    title: "Android"
+  },
+  {
+    image: "flutter.webp",
+    image2: "flutter2.webp",
+    title: "Flutter"
+  },
+  {
+    image: "java.webp",
+    image2: "java2.webp",
+    title: "Java"
+  },
+  {
+    image: "react.webp",
+    image2: "react-white.webp",
+    title: "React"
+  },
+  // Add more items as needed
+];
+export const OPEN_SOURCE_techItems = [
+  {
+    image: "/wordpress.png",
+    image2: "/wordpress2.png",
+    title: "WordPress"
+  },
+  {
+    image: "prestashop.png",
+    image2: "prestashop2.png",
+    title: "Prestashop"
+  },
+  {
+    image: "shopify.webp",
+    image2: "shopify2.webp",
+    title: "Shopify"
+  },
+  // Add more items as needed
+];
 
 export const CUSTOM_TABS_CONTENT = [
   {
@@ -203,77 +204,178 @@ export const CUSTOM_TABS_CONTENT = [
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     backgroundImage: "url('/img2.jpg')",
   },
-];  
-
-
-
-
+];
 
 
 // TEAM MEMBERS DATA
 
 export const teamMembers = [
-    {
-        name: "Jiří Nevařil",
-        bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Jiri_N_1.jpg",
-        description: "CEO, WEBDESIGN"
-    },
-    {
-        name: "Martin Krátký",
-        bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-        description: "CEO, OBCHOD"
-    },
+  {
+    name: "Jiří Nevařil",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Jiri_N_1.jpg",
+    description: "CEO, WEBDESIGN"
+  },
+  {
+    name: "Martin Krátký",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "CEO, OBCHOD"
+  },
 
-    {
-        name: "Pavel Janík",
-        bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Pavel_J_1.jpg",
-        description: "WEBDESIGN"
-    },
-    {
-      name: "Marek S.",
-      bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-      description: "OBCHOD"
+  {
+    name: "Pavel Janík",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Pavel_J_1.jpg",
+    description: "WEBDESIGN"
+  },
+  {
+    name: "Marek S.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "OBCHOD"
   },
   {
     name: "Marek V.",
     bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
     description: "MARKETING"
-},
-{
-  name: "Radek K.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "MARKETING"
-},
-{
-  name: "Andrea G.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "COPYWRITING"
-},
-{
-  name: "Damir K.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "GRAFIKA"
-},
-{
-  name: "Filip K",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "WEBDESIGN"
-},
-{
-  name: "Matyáš F.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "WEBDESIGN"
-},
-{
-  name: "Samuel K.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "WEBDESIGN"
-},
-{
-  name: "Martin B.",
-  bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
-  description: "WEBDESIGN"
-},
+  },
+  {
+    name: "Radek K.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "MARKETING"
+  },
+  {
+    name: "Andrea G.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "COPYWRITING"
+  },
+  {
+    name: "Damir K.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "GRAFIKA"
+  },
+  {
+    name: "Filip K",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "WEBDESIGN"
+  },
+  {
+    name: "Matyáš F.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "WEBDESIGN"
+  },
+  {
+    name: "Samuel K.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "WEBDESIGN"
+  },
+  {
+    name: "Martin B.",
+    bgImage: "https://vstupni.expert-dev.cz/wp-content/uploads/2024/04/Martin_K_1.jpg",
+    description: "WEBDESIGN"
+  },
 
-    // Add more team members as needed
-  ];
+  // Add more team members as needed
+];
+
+
+// Reference Tabs
+export const RefTabs = [
+  { href: "", key: "vse", label: "Vše" },
+  { href: "", key: "weby", label: "Weby" },
+  { href: "", key: "eshopy", label: "E-shopy" },
+];
+
+export const vse = [
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+]
+export const weby = [
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+]
+export const eshopy = [
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+  {
+    name: "Ahome",
+    bgImage: "/reference/eShopy/ahome-cz.jpg",
+    description: "E-SHOPY - SEO - GRAFIKA"
+  },
+]
