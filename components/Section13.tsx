c5e9700083e91c96ba08ac51b410fcb5a8f0fb0a
Atif Zada <116461959+Atifzada@users.noreply.github.com>
2024-05-24 12:09:44 +0000

    complete Section13

---
+++ components/Section13.tsx
@@ -0,0 +1,169 @@
+import React from "react";
+import { LuTriangleRight } from "react-icons/lu";
+
+const Section13 = () => {
+    return (
+        <section className="bg-custom-gradient px-16 pt-20 hidden lg:block">
+            <div className="outermost_most flex flex-col gap-6">
+
+                {/* upper portion */}
+                <div className="flex flex-col gap-2 items-center">
+                    <div className="flex items-center">
+                        <LuTriangleRight className="text-pink" style={{ fill: "#FF0066" }} />
+                        <span className="text-16 text-pink font-QuickSand font-bold pl-2">
+                            BLOG A NOVINKY
+                        </span>
+                    </div>
+                    <div>
+                        <h2 className="text-40 font-extrabold font-Balooc text-black">
+                            Nejnovější články z našeho blogu
+                        </h2>
+                    </div>
+                    <div className="w-1/2 mx-auto text-center">
+                        <p className="text-16 font-QuickSand font-medium text-black">
+                            V našem blogu vám pravidelně přinášíme nejaktuálnější informace, různé tipy a návody ze světa digitálního marketingu a technologií. Prohlédněte si naše nejnovější články a objevte inspiraci pro vaše digitální strategie.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Lower portion/image card portion */}
+                <div className="grid grid-flow grid-cols-4 grid-rows-2 gap-6 min-h-[80vh]">
+                    <div className="col-span-2 flex flex-col gap-6">
+                    <div className="bg-blog1 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[65vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">26. 2. 2024</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/ppc-novinky-2024-co-se-chysta-v-online-marketingu/">
+                                    PPC novinky 2024 – co se chystá v online marketingu?&nbsp;</a>
+                            </h2>
+                        </div>
+                    </div>
+                    
+                    <div className="flex flex-col gap-6">
+                    <div className="bg-blog2 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">24. 1. 2024</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/5-kroku-k-uspesnemu-zviditelneni-vaseho-noveho-e-shopu/  ">
+                                5 kroků k úspěšnému zviditelnění vašeho nového e-shopu &nbsp;</a>
+                            </h2>
+                        </div>
+                        <div>
+                            <p className="text-lb text-14 font-QuickSand font-medium">V dnešní době je online trh přeplněný. Proto je pro každý nový e-shop zásadní vědět, jak vyniknout a zaujmout svou cílovou skupinu. Zviditelnění vašeho e-shopu</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                    <div className="bg-blog3 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">24. 12. 2023</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/5-kroku-k-uspesnemu-zviditelneni-vaseho-noveho-e-shopu/  ">
+                                Jak vytvořit dokonalý Optiweb?   &nbsp;</a>
+                            </h2>
+                        </div>
+                        <div>
+                            <p className="text-lb text-14 font-QuickSand font-medium">V dnešní digitální éře, kde online přítomnost hraje klíčovou roli v úspěchu, je tvorba webových stránek důležitým krokem pro každého, kdo chce oslovit svou cílovou</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                    <div className="bg-blog4 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">7. 12. 2023</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/proc-je-vyhodne-mit-blog-na-vasem-e-shopu/">
+                                Proč je výhodné mít blog na vašem e-shopu? &nbsp;</a>
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                    <div className="bg-blog5 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">27. 11. 2023</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/5-kroku-k-uspesnemu-zviditelneni-vaseho-noveho-e-shopu/  ">
+                                Přinese nadcházející rok revoluci v e-commerce? Hlavní trendy pro e-shopy v roce 2024  &nbsp;</a>
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                    <div className="bg-blog6 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">23. 11. 2023</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/trendy-vyvoje-webu-v-roce-2024-teste-se-na-inovace/">
+                                Trendy vývoje webu v roce 2024: Těšte se na inovace &nbsp;</a>
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-6">
+                        <div className="bg-blog7 bg-no-repeat bg-center rounded-[64px] duration-300 relative border-transparent border-4 hover:border-custom-red cursor-pointer" style={{ borderBottomLeftRadius: "0", backgroundSize: "cover" }}>
+                            <div className="absolute inset-0 bg-custom-red opacity-0 hover:opacity-50 rounded-[60px]" style={{ borderBottomLeftRadius: "0" }}></div>
+                            <div className="min-h-[30vh] flex items-end p-4">
+                                <p className="text-white text-12 font-QuickSand font-medium px-3 py-1 bg-custom-red rounded-full" style={{ borderBottomLeftRadius: "0" }}>BLOG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-custom-red text-14 font-semibold font-QuickSand">13. 11. 2023</span>
+                        </div>
+                        <div>
+                            <h2 className="text-black text-21 font-semibold font-Balooc">
+                                <a href="https://vstupni.expert-dev.cz/navigace-svetem-dotaci-kdo-a-na-co-je-muze-ziskat/">
+                                Navigace světem dotací: Kdo a na co je může získat?&nbsp;</a>
+                            </h2>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Section13;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,7 @@ const config: Config = {
         'pink': '#FF0066',
         'bb': '#333333',
         'p': '#FF006666',
+        'lb': '#777777',
       },
       fontSize: {
         '12': '12px',
@@ -34,6 +35,7 @@ const config: Config = {
         '16': '16px',
         '18': '18px',
         '20': '20px',
+        '21': '21px',
         '22': '22px',
         '24': '24px',
         '28': '28px',
@@ -44,6 +46,13 @@ const config: Config = {
         '76': '76px',
       },
       backgroundImage: {
+        blog1: "url('/PPC-2024-uvodni.jpg')",
+        blog2: "url('/uspesne-zviditelneni-e-shopu.jpg')",
+        blog3: "url('/Optiweb-uvodni-1-1.jpg')",
+        blog4: "url('/Blog-na-e-shopu-uvod.jpg')",
+        blog5: "url('/Trendyv-e-commerce.jpg')",
+        blog6: "url('/Webove-trendy-2024.jpg')",
+        blog7: "url('/Dotace-uvod.jpg')",
         hero: "url('/shape1.png')",
         section4: "url('/O-nas.jpg')",
         video: "url('/video7.jpg')",
@@ -54,9 +63,11 @@ const config: Config = {
         eimg1: "url('/img4.jpg')",
         eimg2: "url('/img4.jpg')",
         eimg3: "url('/img4.jpg')",
+        'custom-gradient': 'linear-gradient(180deg, rgba(255,0,102,0.19) 0%, rgba(255,255,255,1) 21%)',
       }
     }
   },
   plugins: [],
 };
+
 export default config;
