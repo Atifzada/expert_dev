379556571cce28485128bc30afd5be9903052b06
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-05-29 13:11:15 +0500

    Corrected section 3 card image hover

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -14,14 +14,16 @@ const CardComponent: React.FC<CardProps> = ({
   tags,
 }) => {
   return (
-    <div className="relative w-full max-w-lg shadow-lg group">
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-full rounded-3xl transition-transform transform group-hover:scale-105"
-      />
+    <div className="relative w-full max-w-lg shadow-lg">
+      <div className="w-full rounded-3xl cursor-pointer overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full transition-transform transform hover:scale-105 "
+        />
+      </div>
       <div className="absolute bottom-0 left-0 w-full">
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white transition-all group-hover:translate-y-0 flex justify-center w-full">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white transition-all hover:translate-y-0 flex justify-center w-full">
           <div className="custom-class bg-custom-red bg-opacity-100 flex-col justify-center items-center py-8 w-[85%] rounded-full text-center" style={{ borderBottomLeftRadius: "0" }}>
             <h2 className="text-lg font-bold hover:text-white hover:scale-110 transition-transform">{title}</h2>
             <p className="text-sm hover:text-white hover:scale-110 transition-transform">{tags}</p>
@@ -34,7 +36,7 @@ const CardComponent: React.FC<CardProps> = ({
             </div>
           </div>
         </div>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full h-[75%] bg-custom-red bg-opacity-100 w-full rounded-2xl"style={{ borderBottomLeftRadius: "0" }}></div>
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full h-[75%] bg-custom-red bg-opacity-100 w-full rounded-2xl" style={{ borderBottomLeftRadius: "0" }}></div>
       </div>
     </div>
   );
