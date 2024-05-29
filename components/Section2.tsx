d9d011901f7ffac6acdee90ffe86354117d945d2
Atif Zada <atifkhatak242@gmail.com>
2024-05-29 16:54:52 +0500

    Make section responsive

--- components/Section2.tsx
+++ components/Section2.tsx
@@ -65,31 +65,34 @@ const Section2 = () => {
       <div className="relative">
         <div className="inset-0 flex flex-col md:flex-row items-center justify-center z-30">
           {/* <div className="w-[70%] flex flex-col items-center lg:flex-row gap-2 bg-black md:rounded-full lg:rounded-full lg:ml-32 p-6 lg:p-10 border-2 border-custom-red"> */}
-          <div className="ml-28 w-[70%] p-4  grid grid-cols-4 gap-2 bg-black rounded-full">
+          <div className="ml-28 w-[70%] p-4 grid grid-rows-4-col-1 md:grid-cols-4  gap-2 bg-black rounded-full md:max-lg:rounded-[4rem]">
             {/* class_1 */}
             {/* <div className="01 col-span-2 text-white pl-32"> */}
-            <div className="01 flex flex-col justify-center items-center text-white">
+            <div className="01 pt-4 flex flex-col justify-center items-center text-white md:max-lg:col-span-4">
               <h2 className="text-22 font-QuickSand font-extrabold">Expert Dev, s.r.o</h2>
               <p className="text-16 font-QuickSand font-light">vaše digitální agentura</p>
             </div>
 
             {/* class_2 */}
-            <div className="02" >
-              <img src="/section2Overlay.PNG" alt="bg image" 
-              className="rounded-full" 
-              style={{borderTopRightRadius:0,borderBottomRightRadius:0}}
-              />
+            <div className="02 md:max-lg:col-span-4" >
+  <div className="flex w-full">
+    <img 
+      src="/section2Overlay.PNG" 
+      alt="bg image" 
+      className="w-full md:max-lg:h-[3rem] lg:rounded-full lg:rounded-tr-none lg:rounded-br-none overflow-hidden" 
+    />
+  </div>
             </div>
 
             {/* class_3 */}
-            <div className="03 bg-white flex flex-col justify-center items-center px-2">
-            <div><img src="/google_logo.PNG" alt="" /></div>
+            <div className="03 bg-white flex flex-col justify-center items-center px-2 md:max-lg:col-span-2 md:max-lg:rounded-bl-[2.5rem] ">
+            <div className="w-[75%]"><img src="/google_logo.PNG" alt=""/></div>
             <div><p className="text-28 text-black font-QuickSand font-bold">4.9<span className="font-medium">/5</span></p></div>
-            <div><img src="/hodnoceni-5.PNG" alt="" /></div>
+            <div className="w-[75%]"><img src="/hodnoceni-5.PNG" alt="" /></div>
             </div>
 
             {/* Class_4 */}
-            <div className="04 bg-white flex flex-col justify-center items-center gap-4 rounded-full"style={{borderTopLeftRadius:0,borderBottomLeftRadius:0}}>
+            <div className="04 max-md:rounded-bl-full max-md:rounded-br-full bg-white flex flex-col justify-center items-center gap-4 lg:rounded-full lg:rounded-tl-none lg:rounded-bl-none md:max-lg:col-span-2 md:max-lg:rounded-br-[2.5rem]">
               <div><p className="text-14 font-QuickSand font-bold">Programováno</p></div>
               <div><p className="text-28 text-black font-QuickSand font-bold">5,000 h</p></div>
               <div className="psani-text h-10">
