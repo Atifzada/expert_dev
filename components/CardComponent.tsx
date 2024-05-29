806549e84c686a3e365d3768a70f176de99cb2a8
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-05-29 15:41:52 +0500

    Made section 3 card fully responsive

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 interface CardProps {
   title: string;
@@ -13,30 +14,55 @@ const CardComponent: React.FC<CardProps> = ({
   image,
   tags,
 }) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [isRotate, setIsRotate] = useState(false);
+
   return (
     <div className="relative w-full max-w-lg shadow-lg">
       <div className="w-full rounded-3xl cursor-pointer overflow-hidden">
         <img
           src={image}
           alt={title}
-          className="w-full h-full transition-transform transform hover:scale-105 "
+          className="rounded-3xl transition-transform transform hover:scale-105 "
         />
       </div>
-      <div className="absolute bottom-0 left-0 w-full">
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white transition-all hover:translate-y-0 flex justify-center w-full">
-          <div className="custom-class bg-custom-red bg-opacity-100 flex-col justify-center items-center py-8 w-[85%] rounded-full text-center" style={{ borderBottomLeftRadius: "0" }}>
-            <h2 className="text-lg font-bold hover:text-white hover:scale-110 transition-transform">{title}</h2>
-            <p className="text-sm hover:text-white hover:scale-110 transition-transform">{tags}</p>
-            <div className="hidden group-hover:block mt-2">
-              <hr className="border-white w-16 my-2" />
-              <p className="text-xs">{description}</p>
-              <button className="mt-4 bg-black bg-opacity-50 text-white p-2 rounded-full">
-                +
-              </button>
+      <div
+        className={`absolute -bottom-[15%] transform text-white transition-all duration-300 flex justify-center w-full cursor-pointer`}
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+      >
+        <div className="custom-class bg-custom-red bg-opacity-100 flex-col justify-center items-center py-2 sm:py-8 w-[85%] rounded-[2.5rem] sm:rounded-[4.5rem] text-center rounded-bl-none sm:rounded-bl-none">
+          <h2 className="text-[0.5rem] sm:text-[1.125rem] font-bold hover:text-white hover:cursor-pointer transition-transform">{title}</h2>
+          <p className="text-[0.35rem] sm:text-[0.85rem] text-gray-300 hover:text-white hover:cursor-pointer transition-transform">{tags}</p>
+          <div className={`flex flex-col items-center justify-center gap-1 sm:gap-2 transition-transform ${!isHovered ? 'hidden' : 'flex'}`}>
+            <div className="border-t-2 border-white w-[7rem] my-1 sm:my-2" />
+            <p className="text-[0.5rem] sm:text-[0.85rem] text-center w-[80%]">{description}</p>
+            <div
+              className="rounded-[0.25rem] sm:rounded-2xl p-1 sm:p-4 bg-gray-700 flex items-center justify-center"
+              onMouseEnter={(e) => {
+                setIsRotate(true);
+              }}
+              onMouseLeave={(e) => {
+                setIsRotate(false);
+              }}
+            >
+              <FaPlus className={`text-white text-[0.5rem] sm:text-xl cursor-pointer`}
+                onMouseEnter={(e) => {
+                  if (isRotate) {
+                    e.currentTarget.classList.add('rotatePlus');
+                    e.currentTarget.classList.remove('rotateRev');
+                  }
+                }}
+                onMouseLeave={(e) => {
+                  if (isRotate) {
+                    e.currentTarget.classList.remove('rotatePlus');
+                    e.currentTarget.classList.add('rotateRev');
+                  }
+                }} />
             </div>
           </div>
         </div>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full h-[75%] bg-custom-red bg-opacity-100 w-full rounded-2xl" style={{ borderBottomLeftRadius: "0" }}></div>
       </div>
     </div>
   );
