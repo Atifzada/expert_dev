245b6b9dee8afb21e05733ebcc2f59e86435e0e8
Atif Zada <atifkhatak242@gmail.com>
2024-05-29 12:39:20 +0500

    Create Navbar for md and sm devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import { NAV_LINK } from "@/constant";
 import Image from "next/image";
-import { FaPhone } from "react-icons/fa6";
+import { FaPhone, FaFacebook, FaTwitter, FaLinkedin, FaYoutube, FaInstagram, FaPhoneAlt, FaRegBuilding } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { VscThreeBars } from "react-icons/vsc";
 import Link from "next/link";
+import { MdEmail } from "react-icons/md";
 
 const Navbar = () => {
   const [scrolling, setScrolling] = useState(false);
   const [showReferenceDropdown, setShowReferenceDropdown] = useState(false);
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   const referenceLinkRef = useRef<HTMLLIElement>(null);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -46,7 +48,10 @@ const Navbar = () => {
       window.removeEventListener('resize', handleResize); // Clean up on component unmount
     };
   }, []);
-  
+
+  const toggleOverlay = () => {
+    setIsOverlayVisible((prevState) => !prevState);
+  };
 
   return (
     <nav
@@ -91,7 +96,7 @@ const Navbar = () => {
         <div className="hidden lg:flex bg-black text-white rounded-full border border-black p-2 group-hover:bg-custom-red group-hover:border-custom-red">
           <IoSearch className="text-[1rem] cursor-pointer" />
         </div>
-        <VscThreeBars className="text-[2rem] cursor-pointer" />
+        <VscThreeBars className="text-[2rem] cursor-pointer lg:hidden" onClick={toggleOverlay} />
       </div>
       {/* Reference Dropdown */}
       {/* {showReferenceDropdown && referenceLinkRef.current && (
@@ -117,6 +122,96 @@ const Navbar = () => {
           </ul>
         </div>
       )} */}
+
+{isOverlayVisible && (
+  <div className="fixed inset-0 z-40 flex items-start justify-end bg-custom-gr p-8">
+    <div className="w-full h-auto md:mt-2 absolute top-0 right-0 flex flex-col items-start justify-start gap-4 bg-white p-8 rounded-3xl md:w-1/2">
+      <button
+        className="absolute top-4 right-4 text-4xl font-medium text-black"
+        onClick={toggleOverlay}
+      >
+        &times;
+      </button>
+      <Image src="/logo.svg" alt="Logo" width={167} height={40} />
+      <input
+        type="text"
+        placeholder="Vyhledávání"
+        className="w-full mt-4 p-2 border border-gray-300 bg-custom-gray rounded-md"
+      />
+
+      <ul className="flex flex-wrap gap-2 items-center mt-4">
+        {NAV_LINK.map((link, index) => (
+          <li key={link.key} className="mb-2">
+            <Link href={link.href}>
+              <span className="text-14 font-Baloo font-semibold text-black hover:text-custom-red">
+                {link.label}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <hr className="w-full border-custom-gray" />
+
+      <div className="flex flex-col items-start mt-4">
+        <div>
+          <h2 className="text-28 font-Balooc text-custom-red font-extrabold">Kontaktujte O nás</h2>
+        </div>
+
+        <div className="flex gap-2 pt-4">
+          <div className="">
+            <FaPhoneAlt className="text-xl text-custom-red" />
+          </div>
+          <div>
+            <p className="text-black font-normal text-14 font-Baloo">
+              Zavolejte nám
+            </p>
+            <p className="text-black font-medium text-14 font-Baloo">
+              +420 608 657 281
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-2 pt-4">
+          <div className="">
+            <MdEmail className="text-xl text-custom-red" />
+          </div>
+          <div>
+            <p className="text-black font-normal text-14 font-Baloo">
+              Napište nám
+            </p>
+            <p className="text-black font-medium text-14 font-Baloo">
+              info@expert-dev.cz
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-2 pt-4">
+          <div className="">
+            <FaRegBuilding className="text-xl text-custom-red" />
+          </div>
+          <div>
+            <p className="text-black font-normal text-14 font-Baloo">
+              Adresa kanceláře
+            </p>
+            <p className="text-black font-medium text-14 font-Baloo">
+              třída Tomáše Bati 87<br />
+              760 01 Zlín, Česká republika
+            </p>
+          </div>
+        </div>
+        <div className="text-2xl flex mt-4 space-x-2">
+          <a href="#" className="bg-custom-red rounded-full p-2 text-white"><FaFacebook /></a>
+          <a href="#" className="bg-custom-red rounded-full p-2 text-white"><FaTwitter /></a>
+          <a href="#" className="bg-custom-red rounded-full p-2 text-white"><FaLinkedin /></a>
+          <a href="#" className="bg-custom-red rounded-full p-2 text-white"><FaYoutube /></a>
+          <a href="#" className="bg-custom-red rounded-full p-2 text-white"><FaInstagram /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+)}
+
     </nav>
   );
 };
