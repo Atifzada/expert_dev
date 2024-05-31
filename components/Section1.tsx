25f4e74c7d57a93f223dea10a6f9b580f0e141e4
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-05-31 10:12:46 +0500

    Pixel Perfect Section 1 with updated video

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -4,7 +4,7 @@ const Section1 = () => {
   return (
     <div className="flex flex-col items-center justify-end h-[90%] bg-white mt-20">
       <video className="w-[300px] h-[300px] sm:w-full md:h-full" autoPlay muted loop>
-        <source src="https://vstupni.expert-dev.cz/wp-content/uploads/2021/12/FINALNI-VIDEO.mp4" type="video/mp4" />
+        <source src="/video/videoSection.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
